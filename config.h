ab53b93cb7850b27c106a1dc6030f8f73cb0d881
ideas-man <ideasman.git@protonmail.com>
2025-03-10 22:05:04 +0100

    feat: custom config.h

--- config.h
+++ config.h
@@ -4,20 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"xos4 terminus:size=14"
+	"xos4 terminus:size=16"
 };
-
-static const char col_gray1[]  = "#222222";
-static const char col_gray2[]  = "#444444";
-static const char col_gray3[]  = "#bbbbbb";
-static const char col_gray4[]  = "#eeeeee";
-static const char col_red1[]   = "#c10000";
-
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { col_gray4, col_gray1 },
-	[SchemeSel] = { col_gray1, col_gray4 },
+	[SchemeNorm] = { "#eeeeee", "#222222" },
+	[SchemeSel] = { "#222222", "#eeeeee" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
